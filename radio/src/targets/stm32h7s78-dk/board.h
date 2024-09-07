gamegrd/edgetx

WiP: STM32H7S78-DK support

radio/src/targets/stm32h7s78-dk/board.h
@@ -1,0 +1,246 @@
+/*
+ * Copyright (C) EdgeTX
+ *
+ * Based on code named
+ *   opentx - https://github.com/opentx/opentx
+ *   th9x - http://code.google.com/p/th9x
+ *   er9x - http://code.google.com/p/er9x
+ *   gruvin9x - http://code.google.com/p/gruvin9x
+ *
+ * License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#include "definitions.h"
+#include "opentx_constants.h"
+
+#include "board_common.h"
+#include "hal.h"
+#include "hal/serial_port.h"
+#include "hal/watchdog_driver.h"
+
+#define FLASHSIZE                       0x200000
+#define BOOTLOADER_SIZE                 0x20000
+#define FIRMWARE_ADDRESS                0x08000000
+
+#define MB                              *1024*1024
+#define LUA_MEM_EXTRA_MAX               (2 MB)    // max allowed memory usage for Lua bitmaps (in bytes)
+#define LUA_MEM_MAX                     (6 MB)    // max allowed memory usage for complete Lua  (in bytes), 0 means unlimited
+
+extern uint16_t sessionTimer;
+
+#define SLAVE_MODE()                    (g_model.trainerData.mode == TRAINER_MODE_SLAVE)
+
+// Board driver
+void boardInit();
+void boardOff();
+
+// CPU Unique ID
+#define LEN_CPU_UID                     (3*8+2)
+void getCPUUniqueID(char * s);
+
+// Flash Write driver
+#define FLASH_PAGESIZE 256
+void unlockFlash();
+void lockFlash();
+void flashWrite(uint32_t * address, const uint32_t * buffer);
+uint32_t isFirmwareStart(const uint8_t * buffer);
+uint32_t isBootloaderStart(const uint8_t * buffer);
+
+// Pulses driver
+#if !defined(SIMU)
+
+#define INTERNAL_MODULE_ON()            gpio_set(INTMODULE_PWR_GPIO)
+#define INTERNAL_MODULE_OFF()           gpio_clear(INTMODULE_PWR_GPIO);
+#define EXTERNAL_MODULE_ON()            gpio_set(EXTMODULE_PWR_GPIO)
+#define EXTERNAL_MODULE_OFF()           gpio_clear(EXTMODULE_PWR_GPIO)
+#define EXTERNAL_MODULE_PWR_OFF         EXTERNAL_MODULE_OFF
+#define BLUETOOTH_MODULE_ON()           gpio_clear(BLUETOOTH_ON_GPIO)
+#define BLUETOOTH_MODULE_OFF()          gpio_set(BLUETOOTH_ON_GPIO)
+#define IS_INTERNAL_MODULE_ON()         (false)
+#define IS_EXTERNAL_MODULE_ON()         (GPIO_ReadInputDataBit(EXTMODULE_PWR_GPIO, EXTMODULE_PWR_GPIO_PIN) == Bit_SET)
+
+#else
+
+#define INTERNAL_MODULE_OFF()
+#define INTERNAL_MODULE_ON()
+#define EXTERNAL_MODULE_ON()
+#define EXTERNAL_MODULE_OFF()
+#define BLUETOOTH_MODULE_ON()
+#define BLUETOOTH_MODULE_OFF()
+#define IS_INTERNAL_MODULE_ON()         (false)
+#define IS_EXTERNAL_MODULE_ON()         (false)
+
+#endif // defined(SIMU)
+
+#if !defined(NUM_FUNCTIONS_SWITCHES)
+#define NUM_FUNCTIONS_SWITCHES        0
+#endif
+
+#define NUM_TRIMS                       6
+#define DEFAULT_STICK_DEADZONE          2
+
+#define BATTERY_WARN                  37 // 3.7V
+#define BATTERY_MIN                   35 // 3.4V
+#define BATTERY_MAX                   43 // 4.3V
+#define BATTERY_DIVIDER               962
+
+#if defined(__cplusplus) && !defined(SIMU)
+extern "C" {
+#endif
+
+// Power driver
+#define SOFT_PWR_CTRL
+#define POWER_ON_DELAY               10 // 1s
+void pwrInit();
+void extModuleInit();
+uint32_t pwrCheck();
+uint32_t lowPowerCheck();
+
+void pwrOn();
+void pwrSoftReboot();
+void pwrOff();
+void pwrResetHandler();
+bool pwrPressed();
+bool pwrOffPressed();
+#if defined(PWR_EXTRA_SWITCH_GPIO)
+  bool pwrForcePressed();
+#else
+  #define pwrForcePressed() false
+#endif
+uint32_t pwrPressedDuration();;
+  
+const etx_serial_port_t* auxSerialGetPort(int port_nr);
+#define AUX_SERIAL_POWER_ON()
+#define AUX_SERIAL_POWER_OFF()
+
+// LED driver
+void ledInit();
+void ledOff();
+void ledRed();
+void ledBlue();
+void ledGreen();
+
+// LCD driver
+void lcdSetInitalFrameBuffer(void* fbAddress);
+void lcdInit();
+void lcdCopy(void * dest, void * src);
+
+void lcdOff();
+void lcdOn();
+
+#define lcdRefreshWait(...)
+
+// Backlight driver
+#define BACKLIGHT_LEVEL_MAX             100
+#define BACKLIGHT_FORCED_ON             BACKLIGHT_LEVEL_MAX + 1
+#define BACKLIGHT_LEVEL_MIN             1
+
+extern bool boardBacklightOn;
+void backlightLowInit( void );
+void backlightInit();
+void backlightEnable(uint8_t dutyCycle);
+void backlightFullOn();
+bool isBacklightEnabled();
+
+#define BACKLIGHT_ENABLE()                                         \
+  {                                                                \
+    boardBacklightOn = true;                                       \
+    backlightEnable(BACKLIGHT_LEVEL_MAX - currentBacklightBright); \
+  }
+
+#define BACKLIGHT_DISABLE()                                               \
+  {                                                                       \
+    boardBacklightOn = false;                                             \
+    backlightEnable(((g_eeGeneral.blOffBright == BACKLIGHT_LEVEL_MIN) &&  \
+                     (g_eeGeneral.backlightMode != e_backlight_mode_off)) \
+                        ? 0                                               \
+                        : g_eeGeneral.blOffBright);                       \
+  }
+
+#if defined(RADIO_PL18EV)
+#define USB_NAME                        "FlySky PL18EV"
+#define USB_MANUFACTURER                'F', 'l', 'y', 'S', 'k', 'y', ' ', ' '  /* 8 bytes */
+#define USB_PRODUCT                     'P', 'L', '1', '8', 'E', 'V', ' ', ' '  /* 8 Bytes */
+#else
+#define USB_NAME                        "FlySky PL18"
+#define USB_MANUFACTURER                'F', 'l', 'y', 'S', 'k', 'y', ' ', ' '  /* 8 bytes */
+#define USB_PRODUCT                     'P', 'L', '1', '8', ' ', ' ', ' ', ' '  /* 8 Bytes */
+#endif
+
+#if defined(__cplusplus) && !defined(SIMU)
+}
+#endif
+
+// Audio driver
+void audioInit();
+void audioConsumeCurrentBuffer();
+void audioSpiWriteBuffer(const uint8_t * buffer, uint32_t size);
+void audioSpiSetSpeed(uint8_t speed);
+uint8_t audioHardReset();
+uint8_t audioSoftReset();
+void audioSendRiffHeader();
+void audioOn();
+void audioOff();
+bool isAudioReady();
+bool audioChipReset();
+
+#define SPI_SPEED_2                    0
+#define SPI_SPEED_4                    1
+#define SPI_SPEED_8                    2
+#define SPI_SPEED_16                   3
+#define SPI_SPEED_32                   4
+#define SPI_SPEED_64                   5
+#define SPI_SPEED_128                  6
+#define SPI_SPEED_256                  7
+
+#define audioDisableIrq()             // interrupts must stay enabled on Horus
+#define audioEnableIrq()              // interrupts must stay enabled on Horus
+#if defined(PCBNV14)
+#define setSampleRate(freq)
+#else
+void setSampleRate(uint32_t frequency);
+#endif
+void setScaledVolume(uint8_t volume);
+void setVolume(uint8_t volume);
+int32_t getVolume();
+#define VOLUME_LEVEL_MAX               23
+#define VOLUME_LEVEL_DEF               12
+
+// Telemetry driver
+#define INTMODULE_FIFO_SIZE            512
+#define TELEMETRY_FIFO_SIZE            512
+
+// Haptic driver
+void hapticInit();
+void hapticDone();
+void hapticOff();
+void hapticOn(uint32_t pwmPercent);
+
+// Second serial port driver
+//#define AUX_SERIAL
+#define DEBUG_BAUDRATE                  115200
+#define LUA_DEFAULT_BAUDRATE            115200
+
+extern uint8_t currentTrainerMode;
+void checkTrainerSettings();
+
+// Touch panel driver
+bool touchPanelEventOccured();
+struct TouchState touchPanelRead();
+struct TouchState getInternalTouchState();
+
+#define IS_FIRMWARE_COMPATIBLE_WITH_BOARD() true
+
+#endif // _BOARD_H_

radio/src/targets/stm32h7s78-dk/system_init.cpp
@@ -1,0 +1,235 @@
+#include <stdint.h>
+#include "stm32_cmsis.h"
+#include "system_clock.h"
+
+#include "stm32_hal.h"
+// #include "stm32_cmsis.h"
+
+#define NAKED __attribute__((naked))
+
+#if !defined(BOOT)
+#  define BOOTSTRAP __attribute__((section(".bootstrap")))
+#else
+#  define BOOTSTRAP
+#endif
+
+// Linker script symbols
+extern uint32_t _sisr_vector;
+extern uint32_t PSRAM_START;
+
+extern "C" void set_vtor();
+extern "C" void CPU_CACHE_Enable();
+extern "C" void MPU_Init();
+extern "C" void clean_dcache();
+
+#if defined(SDRAM) //TODO: ExtRAM
+// SDRAM driver
+extern "C" int32_t ExtRAM_Init();
+#endif
+
+extern "C" void __libc_init_array();
+extern "C" int main();
+
+extern "C" NAKED BOOTSTRAP
+void Reset_Handler()
+{
+  asm inline (
+    "ldr sp, =_estack \n"
+  );
+  
+#if defined(BOOT)
+  // MCU Init
+  asm inline (
+    "bl SystemInit \n"
+    "bl SystemClock_Config \n"
+// #if defined(SDRAM) // TODO
+//     "bl ExtRAM_Init \n"
+// #endif
+    "bl MPU_Init \n"
+    "bl CPU_CACHE_Enable \n"
+  );
+#endif
+
+#if !defined(BOOT)
+  // Copy / setup ISR vector
+  asm inline (
+    "ldr r0, =_sisr_vector \n"
+    "ldr r1, =_eisr_vector \n"
+    "ldr r2, =_isr_load \n"
+    "bl naked_copy      \n"
+    "bl set_vtor        \n"
+  );
+
+  // Copy code into fast RAM
+  asm inline (
+    "ldr r0, =_siram    \n"
+    "ldr r1, =_eiram    \n"
+    "ldr r2, =_stext_iram \n"
+    "bl naked_copy      \n"
+  );
+
+  // Copy code into normal RAM
+  asm inline (
+    "ldr r0, =_stext    \n"
+    "ldr r1, =_etext    \n"
+    "ldr r2, =_text_load \n"
+    "bl naked_copy      \n"
+  );
+#endif
+
+  // Copy initialized data segment
+  asm inline (
+    "ldr r0, =_sdata    \n"
+    "ldr r1, =_edata    \n"
+    "ldr r2, =_sidata   \n"
+    "bl naked_copy      \n"
+  );
+ 
+  // Zero fill bss segment
+  asm inline (
+    "ldr r2, =_sbss    \n"
+    "ldr r4, =_ebss    \n"
+    "movs r3, 0        \n"
+    "b LoopFillZerobss \n"
+
+  "FillZerobss:        \n"
+    "str  r3, [r2]     \n"
+    "adds r2, r2, 4    \n"
+
+  "LoopFillZerobss:    \n"
+    "cmp r2, r4        \n"
+    "bcc FillZerobss   \n"
+  );
+
+  asm inline (
+    "bl clean_dcache \n"
+    // Call static constructors
+    "bl __libc_init_array \n"
+    "bl SystemCoreClockUpdate \n"
+    // Call the application's entry point
+    "bl main \n"
+    "bx lr   \n"
+  );
+}
+
+extern "C" BOOTSTRAP void set_vtor() {
+  SCB->VTOR = (intptr_t)&_sisr_vector;
+}
+
+extern "C" NAKED BOOTSTRAP void naked_copy() {
+  // r0: destination start
+  // r1: destination end
+  // r2: source start
+  asm inline (
+    "cmp r0, r2         \n"
+    "beq SkipCopy       \n"
+    "movs r3, 0         \n"
+    "b LoopCopyInit     \n"
+
+  "CopyInit:            \n"
+    "ldr r4, [r2, r3]   \n"
+    "str r4, [r0, r3]   \n"
+    "adds r3, r3, #4    \n"
+
+  "LoopCopyInit:        \n"
+    "adds r4, r0, r3    \n"
+    "cmp r4, r1         \n"
+    "bcc CopyInit       \n"
+  "SkipCopy:            \n"
+    "bx lr              \n"
+  );
+}
+
+extern "C" __attribute__((used))
+void CPU_CACHE_Enable()
+{
+  /* Enable I-Cache */
+  SCB_EnableICache();
+
+  /* Enable D-Cache */
+  SCB_EnableDCache();
+}
+
+extern "C" __attribute__((used))
+void MPU_Init()
+{
+  MPU_Region_InitTypeDef MPU_InitStruct = {0};
+
+  /* Disables the MPU */
+  HAL_MPU_Disable();
+
+  /*
+   * Region 0: overwrite default setting to prevent speculative
+   *           read to unavailable memories.
+   * Range: 0x60000000 to 0xE0000000
+   *
+   * Set as strongly ordered, code execution disabled
+   */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.BaseAddress = 0;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
+  MPU_InitStruct.SubRegionDisable = 0x87;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+#if defined(FIRMWARE_QSPI)
+  /* Region 2: QSPI memory range, bank1 */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER2;
+  MPU_InitStruct.BaseAddress = 0x90000000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_8MB;
+  MPU_InitStruct.SubRegionDisable = 0x0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+#endif
+
+#if defined(SDRAM)
+  /* Region 3: SDRAM memory range */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER3;
+  MPU_InitStruct.BaseAddress = (uint32_t)&PSRAM_START;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_8MB;
+  MPU_InitStruct.SubRegionDisable = 0x0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+#endif
+
+  /* Region 3: SRAM1 memory range */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER4;
+  MPU_InitStruct.BaseAddress = 0x30000000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_32KB;
+  MPU_InitStruct.SubRegionDisable = 0x0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
+  /* Enable bus fault exception */
+  SCB->SHCSR |= SCB_SHCSR_BUSFAULTENA_Msk;
+}
+
+extern "C" void clean_dcache() {
+  SCB_CleanDCache();
+}

tiankong52/edge-tx

radio/src/targets/stm32h7s78-dk/board.h
@@ -1,0 +1,249 @@
+/*
+ * Copyright (C) EdgeTX
+ *
+ * Based on code named
+ *   opentx - https://github.com/opentx/opentx
+ *   th9x - http://code.google.com/p/th9x
+ *   er9x - http://code.google.com/p/er9x
+ *   gruvin9x - http://code.google.com/p/gruvin9x
+ *
+ * License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#include "definitions.h"
+#include "edgetx_constants.h"
+
+#include "board_common.h"
+#include "hal.h"
+#include "hal/serial_port.h"
+#include "hal/watchdog_driver.h"
+
+#define FLASHSIZE                       0x200000
+#define BOOTLOADER_SIZE                 0x10000
+#define FIRMWARE_ADDRESS                0x70000000
+#define FIRMWARE_LEN(fsize)             (fsize)
+#define FIRMWARE_MAX_LEN                (FLASHSIZE)
+#define APP_START_ADDRESS               (uint32_t)(FIRMWARE_ADDRESS)
+
+#define MB                              *1024*1024
+#define LUA_MEM_EXTRA_MAX               (2 MB)    // max allowed memory usage for Lua bitmaps (in bytes)
+#define LUA_MEM_MAX                     (6 MB)    // max allowed memory usage for complete Lua  (in bytes), 0 means unlimited
+
+extern uint16_t sessionTimer;
+
+#define SLAVE_MODE()                    (g_model.trainerData.mode == TRAINER_MODE_SLAVE)
+
+// Board driver
+void boardInit();
+void boardOff();
+
+// CPU Unique ID
+#define LEN_CPU_UID                     (3*8+2)
+void getCPUUniqueID(char * s);
+
+// Flash Write driver
+#define FLASH_PAGESIZE 256
+void unlockFlash();
+void lockFlash();
+void flashWrite(uint32_t * address, const uint32_t * buffer);
+uint32_t isFirmwareStart(const uint8_t * buffer);
+uint32_t isBootloaderStart(const uint8_t * buffer);
+
+// Pulses driver
+#if !defined(SIMU)
+
+#define INTERNAL_MODULE_ON()            gpio_set(INTMODULE_PWR_GPIO)
+#define INTERNAL_MODULE_OFF()           gpio_clear(INTMODULE_PWR_GPIO);
+#define EXTERNAL_MODULE_ON()            gpio_set(EXTMODULE_PWR_GPIO)
+#define EXTERNAL_MODULE_OFF()           gpio_clear(EXTMODULE_PWR_GPIO)
+#define EXTERNAL_MODULE_PWR_OFF         EXTERNAL_MODULE_OFF
+#define BLUETOOTH_MODULE_ON()           gpio_clear(BLUETOOTH_ON_GPIO)
+#define BLUETOOTH_MODULE_OFF()          gpio_set(BLUETOOTH_ON_GPIO)
+#define IS_INTERNAL_MODULE_ON()         (false)
+#define IS_EXTERNAL_MODULE_ON()         (GPIO_ReadInputDataBit(EXTMODULE_PWR_GPIO, EXTMODULE_PWR_GPIO_PIN) == Bit_SET)
+
+#else
+
+#define INTERNAL_MODULE_OFF()
+#define INTERNAL_MODULE_ON()
+#define EXTERNAL_MODULE_ON()
+#define EXTERNAL_MODULE_OFF()
+#define BLUETOOTH_MODULE_ON()
+#define BLUETOOTH_MODULE_OFF()
+#define IS_INTERNAL_MODULE_ON()         (false)
+#define IS_EXTERNAL_MODULE_ON()         (false)
+
+#endif // defined(SIMU)
+
+#if !defined(NUM_FUNCTIONS_SWITCHES)
+#define NUM_FUNCTIONS_SWITCHES        0
+#endif
+
+#define NUM_TRIMS                       6
+#define DEFAULT_STICK_DEADZONE          2
+
+#define BATTERY_WARN                  37 // 3.7V
+#define BATTERY_MIN                   35 // 3.4V
+#define BATTERY_MAX                   43 // 4.3V
+#define BATTERY_DIVIDER               962
+
+#if defined(__cplusplus) && !defined(SIMU)
+extern "C" {
+#endif
+
+// Power driver
+#define SOFT_PWR_CTRL
+#define POWER_ON_DELAY               10 // 1s
+void pwrInit();
+void extModuleInit();
+uint32_t pwrCheck();
+uint32_t lowPowerCheck();
+
+void pwrOn();
+void pwrSoftReboot();
+void pwrOff();
+void pwrResetHandler();
+bool pwrPressed();
+bool pwrOffPressed();
+#if defined(PWR_EXTRA_SWITCH_GPIO)
+  bool pwrForcePressed();
+#else
+  #define pwrForcePressed() false
+#endif
+uint32_t pwrPressedDuration();;
+  
+const etx_serial_port_t* auxSerialGetPort(int port_nr);
+#define AUX_SERIAL_POWER_ON()
+#define AUX_SERIAL_POWER_OFF()
+
+// LED driver
+void ledInit();
+void ledOff();
+void ledRed();
+void ledBlue();
+void ledGreen();
+
+// LCD driver
+void lcdSetInitalFrameBuffer(void* fbAddress);
+void lcdInit();
+void lcdCopy(void * dest, void * src);
+
+void lcdOff();
+void lcdOn();
+
+#define lcdRefreshWait(...)
+
+// Backlight driver
+#define BACKLIGHT_LEVEL_MAX             100
+#define BACKLIGHT_FORCED_ON             BACKLIGHT_LEVEL_MAX + 1
+#define BACKLIGHT_LEVEL_MIN             1
+
+extern bool boardBacklightOn;
+void backlightLowInit( void );
+void backlightInit();
+void backlightEnable(uint8_t dutyCycle);
+void backlightFullOn();
+bool isBacklightEnabled();
+
+#define BACKLIGHT_ENABLE()                                         \
+  {                                                                \
+    boardBacklightOn = true;                                       \
+    backlightEnable(BACKLIGHT_LEVEL_MAX - currentBacklightBright); \
+  }
+
+#define BACKLIGHT_DISABLE()                                               \
+  {                                                                       \
+    boardBacklightOn = false;                                             \
+    backlightEnable(((g_eeGeneral.blOffBright == BACKLIGHT_LEVEL_MIN) &&  \
+                     (g_eeGeneral.backlightMode != e_backlight_mode_off)) \
+                        ? 0                                               \
+                        : g_eeGeneral.blOffBright);                       \
+  }
+
+#if defined(RADIO_PL18EV)
+#define USB_NAME                        "FlySky PL18EV"
+#define USB_MANUFACTURER                'F', 'l', 'y', 'S', 'k', 'y', ' ', ' '  /* 8 bytes */
+#define USB_PRODUCT                     'P', 'L', '1', '8', 'E', 'V', ' ', ' '  /* 8 Bytes */
+#else
+#define USB_NAME                        "FlySky PL18"
+#define USB_MANUFACTURER                'F', 'l', 'y', 'S', 'k', 'y', ' ', ' '  /* 8 bytes */
+#define USB_PRODUCT                     'P', 'L', '1', '8', ' ', ' ', ' ', ' '  /* 8 Bytes */
+#endif
+
+#if defined(__cplusplus) && !defined(SIMU)
+}
+#endif
+
+// Audio driver
+void audioInit();
+void audioConsumeCurrentBuffer();
+void audioSpiWriteBuffer(const uint8_t * buffer, uint32_t size);
+void audioSpiSetSpeed(uint8_t speed);
+uint8_t audioHardReset();
+uint8_t audioSoftReset();
+void audioSendRiffHeader();
+void audioOn();
+void audioOff();
+bool isAudioReady();
+bool audioChipReset();
+
+#define SPI_SPEED_2                    0
+#define SPI_SPEED_4                    1
+#define SPI_SPEED_8                    2
+#define SPI_SPEED_16                   3
+#define SPI_SPEED_32                   4
+#define SPI_SPEED_64                   5
+#define SPI_SPEED_128                  6
+#define SPI_SPEED_256                  7
+
+#define audioDisableIrq()             // interrupts must stay enabled on Horus
+#define audioEnableIrq()              // interrupts must stay enabled on Horus
+#if defined(PCBNV14)
+#define setSampleRate(freq)
+#else
+void setSampleRate(uint32_t frequency);
+#endif
+void setScaledVolume(uint8_t volume);
+void setVolume(uint8_t volume);
+int32_t getVolume();
+#define VOLUME_LEVEL_MAX               23
+#define VOLUME_LEVEL_DEF               12
+
+// Telemetry driver
+#define INTMODULE_FIFO_SIZE            512
+#define TELEMETRY_FIFO_SIZE            512
+
+// Haptic driver
+void hapticInit();
+void hapticDone();
+void hapticOff();
+void hapticOn(uint32_t pwmPercent);
+
+// Second serial port driver
+//#define AUX_SERIAL
+#define DEBUG_BAUDRATE                  115200
+#define LUA_DEFAULT_BAUDRATE            115200
+
+extern uint8_t currentTrainerMode;
+void checkTrainerSettings();
+
+// Touch panel driver
+bool touchPanelEventOccured();
+struct TouchState touchPanelRead();
+struct TouchState getInternalTouchState();
+
+#define IS_FIRMWARE_COMPATIBLE_WITH_BOARD() true
+
+#endif // _BOARD_H_

radio/src/targets/stm32h7s78-dk/system_init.cpp
@@ -1,0 +1,235 @@
+#include <stdint.h>
+#include "stm32_cmsis.h"
+#include "system_clock.h"
+
+#include "stm32_hal.h"
+// #include "stm32_cmsis.h"
+
+#define NAKED __attribute__((naked))
+
+#if !defined(BOOT)
+#  define BOOTSTRAP __attribute__((section(".bootstrap")))
+#else
+#  define BOOTSTRAP
+#endif
+
+// Linker script symbols
+extern uint32_t _sisr_vector;
+extern uint32_t PSRAM_START;
+
+extern "C" void set_vtor();
+extern "C" void CPU_CACHE_Enable();
+extern "C" void MPU_Init();
+extern "C" void clean_dcache();
+
+#if defined(SDRAM) //TODO: ExtRAM
+// SDRAM driver
+extern "C" int32_t ExtRAM_Init();
+#endif
+
+extern "C" void __libc_init_array();
+extern "C" int main();
+
+extern "C" NAKED BOOTSTRAP
+void Reset_Handler()
+{
+  asm inline (
+    "ldr sp, =_estack \n"
+  );
+  
+#if defined(BOOT)
+  // MCU Init
+  asm inline (
+    "bl SystemInit \n"
+    "bl SystemClock_Config \n"
+// #if defined(SDRAM) // TODO
+//     "bl ExtRAM_Init \n"
+// #endif
+    "bl MPU_Init \n"
+    "bl CPU_CACHE_Enable \n"
+  );
+#endif
+
+#if !defined(BOOT)
+  // Copy / setup ISR vector
+  asm inline (
+    "ldr r0, =_sisr_vector \n"
+    "ldr r1, =_eisr_vector \n"
+    "ldr r2, =_isr_load \n"
+    "bl naked_copy      \n"
+    "bl set_vtor        \n"
+  );
+
+  // Copy code into fast RAM
+  asm inline (
+    "ldr r0, =_siram    \n"
+    "ldr r1, =_eiram    \n"
+    "ldr r2, =_stext_iram \n"
+    "bl naked_copy      \n"
+  );
+
+  // Copy code into normal RAM
+  asm inline (
+    "ldr r0, =_stext    \n"
+    "ldr r1, =_etext    \n"
+    "ldr r2, =_text_load \n"
+    "bl naked_copy      \n"
+  );
+#endif
+
+  // Copy initialized data segment
+  asm inline (
+    "ldr r0, =_sdata    \n"
+    "ldr r1, =_edata    \n"
+    "ldr r2, =_sidata   \n"
+    "bl naked_copy      \n"
+  );
+ 
+  // Zero fill bss segment
+  asm inline (
+    "ldr r2, =_sbss    \n"
+    "ldr r4, =_ebss    \n"
+    "movs r3, 0        \n"
+    "b LoopFillZerobss \n"
+
+  "FillZerobss:        \n"
+    "str  r3, [r2]     \n"
+    "adds r2, r2, 4    \n"
+
+  "LoopFillZerobss:    \n"
+    "cmp r2, r4        \n"
+    "bcc FillZerobss   \n"
+  );
+
+  asm inline (
+    "bl clean_dcache \n"
+    // Call static constructors
+    "bl __libc_init_array \n"
+    "bl SystemCoreClockUpdate \n"
+    // Call the application's entry point
+    "bl main \n"
+    "bx lr   \n"
+  );
+}
+
+extern "C" BOOTSTRAP void set_vtor() {
+  SCB->VTOR = (intptr_t)&_sisr_vector;
+}
+
+extern "C" NAKED BOOTSTRAP void naked_copy() {
+  // r0: destination start
+  // r1: destination end
+  // r2: source start
+  asm inline (
+    "cmp r0, r2         \n"
+    "beq SkipCopy       \n"
+    "movs r3, 0         \n"
+    "b LoopCopyInit     \n"
+
+  "CopyInit:            \n"
+    "ldr r4, [r2, r3]   \n"
+    "str r4, [r0, r3]   \n"
+    "adds r3, r3, #4    \n"
+
+  "LoopCopyInit:        \n"
+    "adds r4, r0, r3    \n"
+    "cmp r4, r1         \n"
+    "bcc CopyInit       \n"
+  "SkipCopy:            \n"
+    "bx lr              \n"
+  );
+}
+
+extern "C" __attribute__((used))
+void CPU_CACHE_Enable()
+{
+  /* Enable I-Cache */
+  SCB_EnableICache();
+
+  /* Enable D-Cache */
+  SCB_EnableDCache();
+}
+
+extern "C" __attribute__((used))
+void MPU_Init()
+{
+  MPU_Region_InitTypeDef MPU_InitStruct = {0};
+
+  /* Disables the MPU */
+  HAL_MPU_Disable();
+
+  /*
+   * Region 0: overwrite default setting to prevent speculative
+   *           read to unavailable memories.
+   * Range: 0x60000000 to 0xE0000000
+   *
+   * Set as strongly ordered, code execution disabled
+   */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.BaseAddress = 0;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
+  MPU_InitStruct.SubRegionDisable = 0x87;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+#if defined(FIRMWARE_QSPI)
+  /* Region 2: QSPI memory range, bank1 */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER2;
+  MPU_InitStruct.BaseAddress = 0x70000000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_8MB;
+  MPU_InitStruct.SubRegionDisable = 0x0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+#endif
+
+#if defined(SDRAM)
+  /* Region 3: SDRAM memory range */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER3;
+  MPU_InitStruct.BaseAddress = (uint32_t)&PSRAM_START;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_32MB;
+  MPU_InitStruct.SubRegionDisable = 0x0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+#endif
+
+  /* Region 3: SRAM1 memory range */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER4;
+  MPU_InitStruct.BaseAddress = 0x30000000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_32KB;
+  MPU_InitStruct.SubRegionDisable = 0x0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
+  /* Enable bus fault exception */
+  SCB->SHCSR |= SCB_SHCSR_BUSFAULTENA_Msk;
+}
+
+extern "C" void clean_dcache() {
+  SCB_CleanDCache();
+}