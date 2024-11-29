EdgeTX/edgetx

Updated naming of switches and pots according to what is printed in the radio

radio/util/hw_defs/legacy_names.py
@@ -644,14 +644,14 @@ LEGACY_NAMES = [
             "P1": {
                 "yaml": "POT1",
                 "lua": "s1",
-                "label": "S1",
+                "label": "VR1A",
                 "short_label": "1",
                 "description": "Potentiometer 1"
             },
             "P2": {
                 "yaml": "POT2",
                 "lua": "s2",
-                "label": "S2",
+                "label": "VR1B",
                 "short_label": "2",
                 "description": "Potentiometer 2"
             },

tiankong52/edge-tx

radio/util/hw_defs/legacy_names.py
@@ -644,14 +644,14 @@ LEGACY_NAMES = [
             "P1": {
                 "yaml": "POT1",
                 "lua": "s1",
-                "label": "S1",
+                "label": "VR1L",
                 "short_label": "1",
                 "description": "Potentiometer 1"
             },
             "P2": {
                 "yaml": "POT2",
                 "lua": "s2",
-                "label": "S2",
+                "label": "VR1R",
                 "short_label": "2",
                 "description": "Potentiometer 2"
             },